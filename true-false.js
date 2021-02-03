68121e43b3ec48f59044d24fd0acbd7a1a5637ca
bayzed08 <gbir.islam@gmail.com>
2021-02-03 12:27:19 +0600

    JavaScript e

---
+++ true-false.js
@@ -0,0 +1,16 @@
+////False
+//0
+//undefined - not setting variable;
+//""
+//null
+//NaN
+////truthy
+//'0' ' ' 
+let id = 0;
+//let name = "Solaiman Jhan";
+let uname = false;
+if (uname) {
+    console.log("This is true");
+} else {
+    console.log("This is false");
+}
